8e1e430066de65420578e043ca24a754572a820c
Mesud Mujanovic <mesud9494@gmail.com>
2023-07-12 00:42:37 +0200

    start

---
+++ src/test/java/com/Digital/Digital/DigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Digital.Digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
