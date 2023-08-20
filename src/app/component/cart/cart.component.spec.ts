cd75ba06659bebd30b67d11d98dc7a48f97b13b6
Mesud Mujanovic <mesud9494@gmail.com>
2023-08-20 15:05:48 +0200

    cart-jasmin-tests

--- src/app/component/cart/cart.component.spec.ts
+++ src/app/component/cart/cart.component.spec.ts
@@ -3,8 +3,11 @@ import { CartComponent } from './cart.component';
 import { CartService } from 'src/app/service/cart.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CalculatorService } from 'src/app/service/calculator.service';
+import { Observable, of } from 'rxjs';
+import { priceCalculate } from 'src/app/Interface/interPrice';
+import { ITariff } from 'src/app/Interface/ITariff';
 
-describe('CartComponent', () => {
+fdescribe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
   let cartService: CartService;
@@ -33,8 +36,36 @@ describe('CartComponent', () => {
     const router = 1;
     const tariff = 1;
     const calculatedPrice = 2;
-   
-    
-  
+    const mockResponse: priceCalculate = {price:tariff, discount:tariff, calculatedPrice: calculatedPrice}
+    spyOn(calculatorService,'calculateRouterAndTariff').and.returnValue(of(mockResponse));
+    component.routerTariffTotal();
+    expect(component.result).toBe(calculatedPrice);
+  });
+
+  it('should addTariffInCart and return observables', ()=>{
+    const tariff: ITariff ={
+      id:1, name:"tariff", speed:"222", priceList:[
+      {
+        id:1,
+        price:"222",
+        contractDuration: 22,
+        oneTime: false,
+        name:"tariff",
+        discount: 1
+      }
+      ]
+    };
+ 
+   spyOn(cartService,'adaTariffInCart').and.returnValue(of(tariff));
+    const obsFunckiotn = component.addTariffInCart();
+    expect( obsFunckiotn instanceof Observable).toBeTruthy();
+    let result: any;
+    obsFunckiotn.subscribe( (response) =>{
+      result = response;
+    });
+    expect(result).toEqual(tariff);
   })
+
+
+  
 });
